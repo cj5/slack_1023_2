568bf0dcec3b2a7d2d11c809c0eea83a1361e7f9
Chris <stac1221@gmail.com>
2021-08-07 01:31:53 -0500

    Slack challenge

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ const bodyParser = require('body-parser')
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
-app.post('/challenge', (req, res) => {
+app.post('/', (req, res) => {
   console.log(req.body)
   res.send(req.body.challenge)
 })
